b28764c067846995001a5486cb311d7386aa9fde
maryashouri <52279160+maryashouri@users.noreply.github.com>
2020-03-20 23:07:25 +0330

    prettier

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -5,37 +5,41 @@ import * as Ons from 'react-onsenui'
 import 'onsenui/css/onsenui.css'
 import 'onsenui/css/onsen-css-components.css'
 
-const Users=()=> {
-  const [data, loading] = useFetch(
-    "https://reqres.in/api/users?page=1"
-  )
+const Users = () => {
+  const [data, loading] = useFetch('https://reqres.in/api/users?page=1')
 
   return (
-    <Ons.Page renderToolbar={() => <Ons.Toolbar title='لیست افراد' />}>
-    <h1 style={{textAlign:"center"}}>  به اپلیکیشن گل ها خوش آمدید</h1>
-    <div style={{height: 100}}>
-   
-    </div>
-    <Ons.ListTitle>لیست افراد</Ons.ListTitle>
-    <Ons.List modifier="inset" 
-    dataSource={data}
-    renderHeader={() =>
-      <Ons.ListHeader style={{fontSize: 15}} className="testClass"> اسامی </Ons.ListHeader> }
-     renderRow={(user) => (
-      <Ons.ListItem >
-           <Link  props={user} className="center" to={{pathname:`/users/${user.id}`,  state: {
-    user
-  }}}>{user.first_name}</Link>
-     </Ons.ListItem>
-  )}>
-    </Ons.List>
+    <Ons.Page renderToolbar={() => <Ons.Toolbar title="لیست افراد" />}>
+      <h1 style={{ textAlign: 'center' }}> به اپلیکیشن گل ها خوش آمدید</h1>
+      <div style={{ height: 100 }}></div>
+      <Ons.ListTitle> لیست افراد</Ons.ListTitle>
+      <Ons.List
+        modifier="inset"
+        dataSource={data}
+        renderHeader={() => (
+          <Ons.ListHeader style={{ fontSize: 15 }} className="testClass">
+            {' '}
+            اسامی{' '}
+          </Ons.ListHeader>
+        )}
+        renderRow={user => (
+          <Ons.ListItem>
+            <Link
+              props={user}
+              className="center"
+              to={{
+                pathname: `/users/${user.id}`,
+                state: {
+                  user
+                }
+              }}
+            >
+              {user.first_name}
+            </Link>
+          </Ons.ListItem>
+        )}
+      ></Ons.List>
     </Ons.Page>
-  
-);
+  )
 }
 export default Users
-
-
-
-
-
