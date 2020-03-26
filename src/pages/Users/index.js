0e2ed967cc818475e6b5e314398f5cf5a628a8df
maryashouri <52279160+maryashouri@users.noreply.github.com>
2020-03-26 21:29:49 +0430

    add scroll to list

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -6,7 +6,26 @@ import Menu from '../../components/Menu'
 import ThirdPage from '../User/index'
 
 const SecondPage = ({ pushPage, popPage }) => {
-  const [data] = useFetch('https://reqres.in/api/users?page=1')
+  const [data1] = useFetch('https://reqres.in/api/users?page=1')
+  const [data2] = useFetch('https://reqres.in/api/users?page=2')
+  const [data3] = useFetch('https://reqres.in/api/users?page=1')
+  const [data4] = useFetch('https://reqres.in/api/users?page=2')
+  const [data5] = useFetch('https://reqres.in/api/users?page=1')
+  const [data6] = useFetch('https://reqres.in/api/users?page=2')
+  const [data7] = useFetch('https://reqres.in/api/users?page=2')
+  const [data8] = useFetch('https://reqres.in/api/users?page=1')
+
+  const data = [
+    ...data1,
+    ...data2,
+    ...data3,
+    ...data4,
+    ...data5,
+    ...data6,
+    ...data7,
+    ...data8
+  ]
+
   return (
     <Page
       renderToolbar={() => <Menu title="لیست افراد " onBackButton={popPage} />}
