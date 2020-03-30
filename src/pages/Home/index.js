cb0ad7b0f0b30a789303748798b0dc44dd73d8a5
maryashouri <52279160+maryashouri@users.noreply.github.com>
2020-03-30 20:01:14 +0430

    add carousel

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,68 @@
-import React from 'react'
-import { Button, Page } from 'react-onsenui'
+import React, { useState } from 'react'
+import { Button, Page, Carousel, CarouselItem } from 'react-onsenui'
 import SecondPage from '../Users/index'
-import Menu from '../../components/Menu'
 import './style.scss'
 
 const MainPage = ({ pushPage }) => {
+  const initialValue = ['#F1948A', '#D7BDE2', '#85C1E9', '#73C6B6', '#020ca1']
+  const [items, setItems] = useState(initialValue)
+  const [index, setIndex] = useState(0)
+  const handleChange = e => {
+    setIndex(e.activeIndex)
+  }
+
   return (
     <Page>
       <div
         style={{
           textAlign: 'center',
-          margin: 10
+          margin: '0 auto'
         }}
       >
         <h1>به اپلیکیشن گلها خوش آمدید</h1>
-        <Button onClick={() => pushPage(SecondPage, 'افراد')}>افراد</Button>
+        <Button className="btn" onClick={() => pushPage(SecondPage, 'افراد')}>
+          افراد
+        </Button>
+
+        <Carousel
+          onPostChange={handleChange}
+          itemWidth={100}
+          index={index}
+          fullscreen
+          swipeable
+          centered
+          autoScroll
+          overscrollable
+        >
+          {items.map((item, index) => (
+            <CarouselItem key={index} style={{ backgroundColor: item }}>
+              <div style={{ marginTop: '50%', textAlign: 'center' }}>
+                Swipe me!
+              </div>
+            </CarouselItem>
+          ))}
+        </Carousel>
+
+        <div
+          style={{
+            textAlign: 'center',
+            fontSize: '20px',
+            position: 'absolute',
+            bottom: '36px',
+            left: '0px',
+            right: '0px'
+          }}
+        >
+          {items.map((item, index) => (
+            <span
+              key={index}
+              style={{ cursor: 'pointer' }}
+              onClick={() => setIndex(index)}
+            >
+              {index === index ? '\u25CF' : '\u25CB'}
+            </span>
+          ))}
+        </div>
       </div>
     </Page>
   )
