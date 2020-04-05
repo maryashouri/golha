a675e869b22203945fd88546feb47a1f82b529c0
maryashouri <52279160+maryashouri@users.noreply.github.com>
2020-04-05 23:04:42 +0430

    horizontal scroll

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
-import { Button, Page, Carousel, CarouselItem } from 'react-onsenui'
+import React from 'react'
+import { Button, Page } from 'react-onsenui'
 import SecondPage from '../Users/index'
 import './style.scss'
 
 const MainPage = ({ pushPage }) => {
-  const initialValue = ['#F1948A', '#D7BDE2', '#85C1E9', '#73C6B6', '#020ca1']
-  const [items, setItems] = useState(initialValue)
-  const [index, setIndex] = useState(0)
-  const handleChange = e => {
-    setIndex(e.activeIndex)
-  }
+  const colorArray = [
+    '#F1948A',
+    '#D7BDE2',
+    '#85C1E9',
+    '#73C6B6',
+    '#020ca1',
+    '#D7BDE2',
+    '#85C1E9',
+    '#73C6B6',
+    '#020ca1'
+  ]
 
   return (
     <Page>
@@ -23,46 +28,17 @@ const MainPage = ({ pushPage }) => {
         <Button className="btn" onClick={() => pushPage(SecondPage, 'افراد')}>
           افراد
         </Button>
-
-        <Carousel
-          onPostChange={handleChange}
-          itemWidth={100}
-          index={index}
-          fullscreen
-          swipeable
-          centered
-          autoScroll
-          overscrollable
-        >
-          {items.map((item, index) => (
-            <CarouselItem key={index} style={{ backgroundColor: item }}>
-              <div style={{ marginTop: '50%', textAlign: 'center' }}>
-                Swipe me!
-              </div>
-            </CarouselItem>
-          ))}
-        </Carousel>
-
-        <div
-          style={{
-            textAlign: 'center',
-            fontSize: '20px',
-            position: 'absolute',
-            bottom: '36px',
-            left: '0px',
-            right: '0px'
-          }}
-        >
-          {items.map((item, index) => (
-            <span
+      </div>
+      <div className="slider">
+        {colorArray.map((value, index) => {
+          return (
+            <div
               key={index}
-              style={{ cursor: 'pointer' }}
-              onClick={() => setIndex(index)}
-            >
-              {index === index ? '\u25CF' : '\u25CB'}
-            </span>
-          ))}
-        </div>
+              className="slider-box"
+              style={{ backgroundColor: value }}
+            ></div>
+          )
+        })}
       </div>
     </Page>
   )
