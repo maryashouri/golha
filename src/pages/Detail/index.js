7d982312ffa28346220df168f01b8b1b181c9fa7
maryashouri <52279160+maryashouri@users.noreply.github.com>
2020-04-14 16:21:55 +0430

    remove player from this  branch

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Button, Page } from 'react-onsenui'
-import Player from '../../components/player/index'
 
 const Detail = ({ data, popPage }) => {
   return (
@@ -11,7 +10,7 @@ const Detail = ({ data, popPage }) => {
         }}
       >
         <h2>{data}</h2>
-        <Player></Player>
+
         <Button className="back-btn" onClick={popPage}>
           بازگشت
         </Button>
