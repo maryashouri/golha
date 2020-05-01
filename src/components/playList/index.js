522e7743944c8fea2ecabf40186ebde886cf61bc
maryashouri <52279160+maryashouri@users.noreply.github.com>
2020-05-01 19:52:04 +0430

    add playlist

--- src/components/playList/index.js
+++ src/components/playList/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { LazyList, ListItem } from 'react-onsenui'
+import React, { useState } from 'react'
+import { LazyList, ListItem, Button } from 'react-onsenui'
+import AudioPlayer from 'react-playlist-player'
 
 import './style.scss'
 
@@ -9,16 +10,43 @@ const PlayList = ({ item }) => {
       id: 1,
       persianTitle: 'رضا  صادقی',
       title: 'sadeghi',
-      trackName: 'یه چیزی میشه دیگه'
+      trackName: 'یه چیزی میشه دیگه',
+      src:
+        'https://dls.music-fa.com/tagdl/99/Reza%20Sadeghi%20-%20Ye%20Chizi%20Mishe%20Dige%20(128).mp3'
     },
     {
       id: 2,
       persianTitle: 'همایون شجریان',
       title: 'homayoon',
-      trackName: 'ابر میبارد'
+      trackName: 'ابر میبارد',
+      src:
+        'https://dls.music-fa.com/tagdl/downloads/Homayoun%20Shajarian%20-%20Abr%20Mibarad%20(128).mp3'
     },
-    { id: 3, persianTitle: 'گوگوش', title: 'gogoosh', trackName: 'دو پنجره' }
+    {
+      id: 3,
+      persianTitle: 'گوگوش',
+      title: 'gogoosh',
+      trackName: 'دو پنجره',
+      src:
+        'http://dl.vblogit.ir/googoosh/Googoosh%20-%202%20Panjereh/06%20-%202%20Panjereh.mp3'
+    }
   ]
+
+  const addTrackToPlaylist = track => {
+    setArr(arr.concat({ songUrl: track.src, songName: track.title }))
+  }
+  const [currentPlayList, setCurrentPlayList] = useState({})
+  const [arr, setArr] = useState([])
+  const loadPlayList = () => {
+    setCurrentPlayList({
+      playlistCoverUrl:
+        'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+      playlistName: 'playlist name',
+      bandName: 'band name',
+      songs: arr,
+      type: 'album'
+    })
+  }
 
   return (
     <div style={{ textAlign: 'center' }}>
@@ -26,16 +54,30 @@ const PlayList = ({ item }) => {
         modifier="inset"
         length={list.length}
         renderRow={index => (
-          <ListItem onClick={() => item(list[index].title)} key={index}>
+          <ListItem onClick={() => item(list[index].src)} key={index}>
             <div className="list-item">
-              <div> {list[index].trackName}</div>
-              <div className="list-item__singer">
+              <div className="list-item__detail"> {list[index].trackName}</div>
+              <Button
+                modifier="material--flat"
+                onClick={() => addTrackToPlaylist(list[index])}
+              >
+                {' '}
+                ...افزودن به لیست
+              </Button>
+              <div className="list-item__singer list-item__detail">
                 {list[index].persianTitle}
               </div>
             </div>
           </ListItem>
         )}
         calculateItemHeight={() => 44}
+      />
+      <Button className={'Demo__load-btn'} onClick={loadPlayList}>
+        load playlist
+      </Button>
+      <AudioPlayer
+        currentPlayList={currentPlayList}
+        onToggle={({ audioPlaying }) => console.log({ audioPlaying })}
       />
     </div>
   )
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,21 +1,21 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 export function usePlayer(ref) {
-  const [selectedTrack, setSelectedTrack] = useState('sa')
+  const [selectedTrack, setSelectedTrack] = useState({})
   const [player, setPlayer] = useState('stopped')
   const [features, setFeatures] = useState({ currentTime: '', duration: '' })
 
-  const usePrevious = value => {
-    // The ref object is a generic container whose current property is mutable ...
-    // ... and can hold any value, similar to an instance property on a class
-    const ref = useRef()
-    // Store current value in ref
-    useEffect(() => {
-      ref.current = value
-    }, [value]) // Only re-run if value changes
-    // Return previous value (happens before update in useEffect above)
-    return ref.current
-  }
-  const prevPlayer = usePrevious(player)
+  // const usePrevious = value => {
+  //   // The ref object is a generic container whose current property is mutable ...
+  //   // ... and can hold any value, similar to an instance property on a class
+  //   const ref = useRef()
+  //   // Store current value in ref
+  //   useEffect(() => {
+  //     ref.current = value
+  //   }, [value]) // Only re-run if value changes
+  //   // Return previous value (happens before update in useEffect above)
+  //   return ref.current
+  // }
+  // const prevPlayer = usePrevious(player)
   useEffect(() => {
     ref.current.addEventListener('timeupdate', e => {
       setFeatures({
@@ -26,27 +26,10 @@ export function usePlayer(ref) {
     return () => ref.current.removeEventListener('timeupdate', () => {})
   }, [])
   useEffect(() => {
-    const sadeghi =
-      'https://dls.music-fa.com/tagdl/99/Reza%20Sadeghi%20-%20Ye%20Chizi%20Mishe%20Dige%20(128).mp3'
-    const homayoon =
-      'https://dls.music-fa.com/tagdl/downloads/Homayoun%20Shajarian%20-%20Abr%20Mibarad%20(128).mp3'
-    const gogoosh =
-      'http://dl.vblogit.ir/googoosh/Googoosh%20-%202%20Panjereh/06%20-%202%20Panjereh.mp3'
     let track
-
-    switch (selectedTrack) {
-      case 'sadeghi':
-        track = sadeghi
-        break
-      case 'homayoon':
-        track = homayoon
-        break
-      case 'gogoosh':
-        track = gogoosh
-      default:
-        break
+    if (typeof selectedTrack === 'string') {
+      track = selectedTrack
     }
-
     if (track) {
       ref.current.src = track
       setTimeout(() => {
